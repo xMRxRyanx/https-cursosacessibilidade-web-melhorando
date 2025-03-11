aed49a56664d6aea53371a1a330e72d165e3d4f1
xMRxRyanx <161638993+xMRxRyanx@users.noreply.github.com>
2025-03-11 16:10:27 -0300

    11/03/25

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click',function() {
-        tamanhoAtual +=0.1;
+        tamanhoAtualFonte +=0.1;
          document.body.style.fontSize= `${tamanhoAtualFonte}rem`
     })
 
