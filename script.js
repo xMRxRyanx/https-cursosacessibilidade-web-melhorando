444410447880ed98b86d720d1fa9e29f1665853b
xMRxRyanx <161638993+xMRxRyanx@users.noreply.github.com>
2025-04-01 14:38:01 -0300

    04

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 const botaoDeAcessibilidade= document.getElementById('botao-acessibilidade')
 const opcoesDeAcessibilidade= document.getElementById('opcoes-acessibilidade')
 const alternaContraste=document.getElementById('anterna-contraste')
 
-botaoDeAcessibilidade.addEventListener('click',function () {
+botaoDeAcessibilidade.addEventListener('click',function() {
 botaoDeAcessibilidade.classList.toggle('rotacao-botao')
 opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 })
