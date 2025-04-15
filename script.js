a3249dceeb0e0a191abb19484df572392a918df2
xMRxRyanx <161638993+xMRxRyanx@users.noreply.github.com>
2025-04-15 16:00:03 -0300

    14/04

--- script.js
+++ script.js
@@ -30,3 +30,6 @@ alternaContraste.addEventListener('click',function(){
 })
 })
 ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
